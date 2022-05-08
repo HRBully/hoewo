107255742be7740588a4444a9b9cda2c7d0f07fb
hogskin <hogskin@sylu.edu.cn>
2022-05-08 21:22:50 +0800

    :sparkles:文章详情

---
+++ pages/cont/cont.js
@@ -0,0 +1,83 @@
+// pages/cont/cont.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+
+    },
+    getcontent() {
+        let { title, chapter } = this.data
+        wx.cloud.database().collection('contents').where({
+            title,
+            chapter
+        }).get().then(res => {
+            console.log(res)
+            this.setData({
+                content: res.data[0].content
+            })
+            this.selectComponent('#preContent').build();
+        })
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        let { title, chapter } = options
+        this.setData({
+            title,
+            chapter
+        })
+        this.getcontent()
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
