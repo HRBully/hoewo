ded337272e38c3e23005751e3e290d339088b050
shancha <1206260484@qq.com>
2022-05-15 10:28:22 +0800

    获取季节

--- app.js
+++ app.js
@@ -1,33 +1,52 @@
 // app.js
 App({
-  // 小程序启动执行
-  globalData: {
-    loginStatus: false,
-    selected: 0,// tabbar跳转页面index
-    season:''
-  },
-  onLaunch() {
-      
-    console.log("小程序启动")
-    wx.cloud.init({
-      // 云开发环境id
-      env: 'helpfarmers-5g7z61ck3843e204',
-      traceUser: true,
-    })
-    let capsuleObj = wx.getMenuButtonBoundingClientRect();
-    // console.log("==胶囊信息==");
-    // console.log(capsuleObj);
-    wx.getSystemInfo({
-      success: (res) => {
-        // console.log("==获取系统信息==");
-        // console.log(res)
-        var statusBarHeight = res.statusBarHeight; //顶部状态栏高度
-        this.globalData.capsuleObj = capsuleObj;
-        this.globalData.titleHeight = statusBarHeight + capsuleObj.height + (capsuleObj.top - statusBarHeight) * 2;
-      },
-      failure() {}
-    })
-    
-  },
+    // 小程序启动执行
+    globalData: {
+        loginStatus: false,
+        season: 'spring'
+    },
+    // 获取季节
+    getSeason() {
+        let date = new Date()
+        let month = date.getMonth() + 1
+        switch (month) {
+            case month > 2 && month < 6:
+                this.globalData.season = 'spring'
+                break;
+            case month > 5 && month < 9:
+                this.globalData.season = 'summer'
+                break;
+            case month > 8 && month < 12:
+                this.globalData.season = 'autumn'
+                break;
+            case month > 11 || month <3:
+                this.globalData.season = 'winter'
+                break;
+        }
+
+    },
+    onLaunch() {
+        this.getSeason()
+        console.log("小程序启动")
+        wx.cloud.init({
+            // 云开发环境id
+            env: 'helpfarmers-5g7z61ck3843e204',
+            traceUser: true,
+        })
+        let capsuleObj = wx.getMenuButtonBoundingClientRect();
+        // console.log("==胶囊信息==");
+        // console.log(capsuleObj);
+        wx.getSystemInfo({
+            success: (res) => {
+                // console.log("==获取系统信息==");
+                // console.log(res)
+                var statusBarHeight = res.statusBarHeight; //顶部状态栏高度
+                this.globalData.capsuleObj = capsuleObj;
+                this.globalData.titleHeight = statusBarHeight + capsuleObj.height + (capsuleObj.top - statusBarHeight) * 2;
+            },
+            failure() {}
+        })
+
+    },
 
 })
--- cloud/home/index.js
+++ cloud/home/index.js
@@ -12,7 +12,6 @@ exports.main = async (event, context) => {
     // 获取节气
     app.router('getSolarTerm', async (ctx, next) => {
         let currentTime = event.currentTime
-        console.log(currentTime);
         let solarTerm = await db.collection('solar-term').where({
             time: _.gte(currentTime)
         }).limit(1).get()
@@ -21,15 +20,16 @@ exports.main = async (event, context) => {
     // 获取咨询信息
     app.router('getConsult', async (ctx, next) => {
         // 下拉刷新次数
-        let account = event.account
-        let result = await db.collection('consult').skip(account * 10).limit(10).get()
+        let account = event.accountConsult
+        console.log(account);
+        let result = await db.collection('consult').skip(account * 5).limit(5).get()
         ctx.body = result
     })
     // 获取新闻信息
     app.router('getNews', async (ctx, next) => {
         // 下拉刷新次数
-        let account = event.account
-        let result = await db.collection('news').skip(account * 10).limit(10).get()
+        let account = event.accountNews
+        let result = await db.collection('news').skip(account * 5).limit(5).get()
         ctx.body = result
     })
     return app.serve()
